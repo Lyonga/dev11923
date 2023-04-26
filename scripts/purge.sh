ef43de4946b57ea0fec48f7352e76cd4e5a5bb38
Lyonga <60431189+Lyonga@users.noreply.github.com>
2023-04-26 10:42:38 -0400

    Create purge.sh

---
+++ scripts/purge.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Get the current timestamp
+now=$(date +%s)
+
+# Loop through all remote branches
+for branch in $(git branch -r --no-merged); do
+  # Check if the branch matches the excluded patterns
+  if [[ $branch == origin/main* || $branch == origin/dev* || $branch == origin/prod* ]]; then
+    continue
+  fi
+  # Check if the branch is older than 90 days
+  last_commit=$(git log --format="%ct" -n 1 $branch)
+  if (( now - last_commit > 90*24*60*60 )); then
+    # Delete the branch if it has no logs or commits in the last 90 days
+    if [[ $(git log $branch) == "" ]]; then
+      git push origin --delete $branch
+      echo "Deleted branch $branch"
+    fi
+  fi
+done
