b2d638766259aaf7c1fb1a7ec16d9846c8074100
Lyonga <60431189+Lyonga@users.noreply.github.com>
2023-04-26 10:05:29 -0400

    Update delete-stale-branch.sh

--- delete-stale-branch.sh
+++ delete-stale-branch.sh
@@ -3,7 +3,7 @@
 set -e
 
 for branch in $(git branch -a | sed 's/^\s*//' | sed 's/^remotes\///' | grep -v 'master$'); do
-  if [[ "$(git log $branch --since "April 02, 2023" | wc -l)" -eq 0 ]]; then
+  if [[ "$(git log --since "April 02, 2023" $branch | wc -l)" -eq 0 ]]; then
     local_branch_name=$(echo "$branch" | sed 's/remotes\/origin\///')
     echo "Deleting branch $local_branch_name..."
     git push --delete origin "$local_branch_name"
